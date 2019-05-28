e09bdaeeac44dea2a1c2f895434e4295762fa217
Prashant Jha <prashantjha.jec@gmail.com>
2019-05-28 16:00:08 +0530

    Added Comments.

--- g.py
+++ g.py
@@ -1,5 +1,8 @@
+# program to tell types of all characters from a string
+
 def fun(a):
     for i in range(len(a)):
+        # using ascii value of characters to check type of character
         if (ord(a[i])>=ord('a') and ord(a[i])<=ord('z')) or (ord(a[i])>=ord('A') and ord(a[i])<=ord('Z')):
             print(a[i],' is a character')
 
@@ -9,6 +12,7 @@ def fun(a):
         elif ord(a[i])>20:
             print(a[i],' is a special character.')
 
+# taking input from user as a string.
 s = input("Enter a string with all kind of characters : ")
 a = list(s)
 fun(a)
