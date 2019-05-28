ed8d43826f881439322bdef6bfcbacdcd79c5a92
Prashant Jha <prashantjha.jec@gmail.com>
2019-05-28 16:05:41 +0530

    added comments

--- i.py
+++ i.py
@@ -1,5 +1,8 @@
+# program to delete all .xlsx files from current working dir.
+# impoting libraries
 import os
 
+# this function will remove all files with extension .xlsx
 def rmv_all(suf = '.xlsx'):
     lst = os.listdir()
     a = [x for x in lst if x.endswith(suf)]
