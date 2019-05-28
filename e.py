c9645c251c3a4be96f5c2256e601bb7641df231d
Prashant Jha <prashantjha.jec@gmail.com>
2019-05-28 15:51:50 +0530

    Added comments.

--- e.py
+++ e.py
@@ -1,5 +1,8 @@
+# program to make .csv file from .xlsx in curren working directory.
+# importing pandas library
 import pandas as pd
 
+# this function will read .xlsx from current working directory and write .csv.
 def xl2csv(a):
     xl = pd.read_excel('mart.xlsx')
     xl.to_csv('mart_csv.csv', index = False, encoding = 'utf-8')
