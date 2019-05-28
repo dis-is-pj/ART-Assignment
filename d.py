a1ae211913d85bbf5de089ed91d6afe23dc96586
Prashant Jha <prashantjha.jec@gmail.com>
2019-05-28 15:48:27 +0530

    Added comments to make the code  more readable.

--- d.py
+++ d.py
@@ -1,3 +1,5 @@
+# function to extract the numeric value from a string
+# considering there is only one numeric value in given string
 def fun(s):
     a = ''
     f = 0
@@ -7,6 +9,8 @@ def fun(s):
             a = s[i]
         elif f==1 and (ord(s[i])>=ord('0') and ord(s[i])<=ord('9')):
             a = a + s[i]
+    # if there are more than one numeric value in the string
+    # the function will only return the value appeared first from left
     return int(a)
 
 s = input('Please enter a string\n')
