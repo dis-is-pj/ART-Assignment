33ab332eacd52c4c3dfffb2dac88832256b86ad1
Prashant Jha <prashantjha.jec@gmail.com>
2019-05-28 16:03:39 +0530

    Added comments

--- h.py
+++ h.py
@@ -1,4 +1,13 @@
+# program to create a .xlsx file in current working dir.
+
+# importing the libraries
 from xlwt import Workbook as wb
-wb = wb()
+
+# creating the empty .xlsx file
+wb = wb()  
+
+# adding the sheet named 'test' in .xlsx file.
 sheet1 = wb.add_sheet('test')
+
+# saving the xlsx file in current working directory with name example.
 wb.save('example.xlsx')
