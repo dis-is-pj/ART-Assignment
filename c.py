2d71321a2b6888f653b20273aa5a8415e1618fb6
Prashant Jha <prashantjha.jec@gmail.com>
2019-05-28 15:43:05 +0530

    Added comments for more readable code.

--- c.py
+++ c.py
@@ -1,7 +1,9 @@
-def fun(s):
+# function to remove all the digits and special characters
+def fun(s):  # parameter s : string
     a = list(s)
     i = 0
     while(i<len(a)):
+        # using ascii value to check type of character
         if not((ord(a[i])<=ord('z') and ord(a[i])>=ord('a'))
         or (ord(a[i])<=ord('z') and ord(a[i])>=ord('a'))):
             a.pop(i)
@@ -10,5 +12,6 @@ def fun(s):
 
     return ''.join(a)
 
-s = input('Please enter a string\n')
+# Taking user input
+s = input('Please enter a string\n') 
 print(fun(s))
